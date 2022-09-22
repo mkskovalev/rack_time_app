50fc594bc8f86bdd27b59d2bdb99882b517c88c9
mkskovalev <maxonemore@ya.ru>
2022-09-23 01:20:49 +0700

    did the map url_formats  one time

--- time_formatter.rb
+++ time_formatter.rb
@@ -14,27 +14,30 @@ class TimeFormatter
     params = params
     @separator = params[/\W|[_]/]
     @url_formats = params.split(@separator)
-    @unknown = unknown_formats
   end
 
   def success?
+    @converted_formats = @url_formats.map {
+      |value| FORMATS[value.to_sym].nil? ? value : FORMATS[value.to_sym]
+    }
+    @unknown = unknown_formats
     @unknown.length == 0
   end
 
   def formatted_time
-    final_format = convert_url_formats('formatted').join(@separator)
+    final_format = @converted_formats.join(@separator)
     Time.now.strftime(final_format)
   end
 
   private
 
   def unknown_formats
-    unknown = convert_url_formats('unknown')
-    FORMATS.each_key { |key| unknown.delete(key.to_s) }
+    unknown = @converted_formats.map { |value| value }
+    FORMATS.each_value { |value| unknown.delete(value.to_s) }
     return unknown
   end
 
   def convert_url_formats(method)
-    @url_formats.map { |value| method == 'formatted' ? FORMATS[value.to_sym] : value }
+
   end
 end
